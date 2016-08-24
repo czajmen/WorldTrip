faac5ed559e5435224676e25dd484a45f9b1986b
czajmen <kamil.czaja94@gmail.com>
2016-08-24 15:42:42 +0200

    AppController

---
+++ TheWorld/src/TheWorld/wwwroot/js/site.js
@@ -0,0 +1,41 @@
+﻿
+
+(function () {
+
+   
+    //var ele = $("#username");
+    //ele.text("czajmen");
+
+    //var main = $("main");
+
+    //main.on("mouseenter" , function () {
+    //    main.style.backgroundColor = "#888";
+    //});
+    //main.on("mouseleave" , function () {
+    //    main.style.backgroundColor = "";
+    //});
+
+    //var menuItems = $("ul.menu li a");
+    //menuItems.on("click", function () {
+    //    var me = $(this);
+
+    //    alert(me.text());
+    //});
+
+
+    var $sideBarAndWrapper = $("#sidebar , #wrapper");
+
+    $("#sideBarToggle").on("click", function () {
+        $sideBarAndWrapper.toggleClass("hide-sidebar");
+
+        if($sideBarAndWrapper.hasClass("hide-sidebar"))
+        {
+            $(this).text("Wyświetl Menu");
+        }
+        else {
+            $(this).text("Ukryj Menu");
+        }
+
+    });
+})();
+
